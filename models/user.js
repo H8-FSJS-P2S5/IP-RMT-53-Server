d08905fd0397f531ed18eace49f3597c6e705d5d
RenFrost <diidnugroho@gmail.com>
2024-09-30 20:05:36 +0700

    feat: add register endpoint

--- models/user.js
+++ models/user.js
@@ -1,15 +1,10 @@
 "use strict";
 const { Model } = require("sequelize");
 const { hashPassword } = require("../helpers/bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       User.hasMany(models.AnimeList, {
         foreignKey: "userId",
         onDelete: "CASCADE",
@@ -24,11 +19,41 @@ module.exports = (sequelize, DataTypes) => {
       });
     }
   }
+
   User.init(
     {
-      username: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Username is required." },
+          len: {
+            args: [3, 20],
+            msg: "Username must be between 3 and 20 characters.",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Email is required." },
+          isEmail: {
+            msg: "Email must be a valid email address.",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Password is required." },
+          len: {
+            args: [6, 100],
+            msg: "Password must be at least 6 characters long.",
+          },
+        },
+      },
     },
     {
       sequelize,
@@ -40,5 +65,6 @@ module.exports = (sequelize, DataTypes) => {
       },
     }
   );
+
   return User;
 };

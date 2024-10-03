e9d90305db9f17396dfd3f097ebbf0c5765a1e76
RenFrost <diidnugroho@gmail.com>
2024-10-03 20:41:37 +0700

    chore: modifying cors to allow all origin

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const router = require('./routes')
-const errorHandler = require('./middlewares/errorHandler')
-var cors = require('cors')
+const express = require("express");
+const app = express();
+const router = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
+var cors = require("cors");
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "*", // Allows all origins
+  })
+);
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.use(router)
+app.use(router);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-module.exports=app;
+module.exports = app;

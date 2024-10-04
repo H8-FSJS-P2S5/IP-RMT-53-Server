10e03867703e6d554e001c01e2c1ed3b204368d4
RenFrost <diidnugroho@gmail.com>
2024-10-04 09:21:00 +0700

    refactor: moving fetching data from 3rd API to client-side

--- controllers/AnimeController.js
+++ controllers/AnimeController.js
@@ -2,36 +2,34 @@ const { Anime } = require("../models");
 const axios = require("axios");
 
 class AnimeController {
-  
   static async searchAnime(req, res, next) {
     const { q } = req.query;
 
     try {
-      
       const { data } = await axios.get("https://api.jikan.moe/v4/anime", {
         params: { q },
       });
 
-      const animeIds = data.data.map(anime => anime.mal_id); 
+      const animeIds = data.data.map((anime) => anime.mal_id);
 
-      
       const existingAnime = await Anime.findAll({
         where: {
           malId: animeIds,
         },
-        attributes: ["malId"], 
+        attributes: ["malId"],
       });
 
-      const existingIds = existingAnime.map(anime => anime.malId); 
+      const existingIds = existingAnime.map((anime) => anime.malId);
 
-      const newAnime = data.data.filter(anime => !existingIds.includes(anime.mal_id));
+      const newAnime = data.data.filter(
+        (anime) => !existingIds.includes(anime.mal_id)
+      );
 
-      
       if (newAnime.length > 0) {
-        const animeToCreate = newAnime.map(anime => ({
+        const animeToCreate = newAnime.map((anime) => ({
           malId: anime.mal_id,
           title: anime.title,
-          genre: anime.genres.map(g => g.name).join(", "),
+          genre: anime.genres.map((g) => g.name).join(", "),
           synopsis: anime.synopsis,
           episodes: anime.episodes,
           imageUrl: anime.images.jpg.image_url,
@@ -41,7 +39,6 @@ class AnimeController {
         await Anime.bulkCreate(animeToCreate);
       }
 
-    
       res.status(200).json({
         message: "Anime data fetched and stored from Jikan successfully",
         data: data.data,
@@ -51,7 +48,45 @@ class AnimeController {
     }
   }
 
-  
+  static async storeAnime(req, res, next) {
+    const animeData = req.body;
+
+    try {
+      const animeIds = animeData.map((anime) => anime.mal_id);
+
+      const existingAnime = await Anime.findAll({
+        where: { malId: animeIds },
+        attributes: ["malId"],
+      });
+
+      const existingIds = existingAnime.map((anime) => anime.malId);
+
+      const newAnime = animeData.filter(
+        (anime) => !existingIds.includes(anime.mal_id)
+      );
+
+      if (newAnime.length > 0) {
+        const animeToCreate = newAnime.map((anime) => ({
+          malId: anime.mal_id,
+          title: anime.title,
+          genre: anime.genres.map((g) => g.name).join(", "),
+          synopsis: anime.synopsis,
+          episodes: anime.episodes,
+          imageUrl: anime.images.jpg.image_url,
+          score: anime.score,
+        }));
+
+        await Anime.bulkCreate(animeToCreate);
+      }
+
+      res.status(200).json({
+        message: "Anime data stored successfully",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
   static async getAnimeDetails(req, res, next) {
     const { id } = req.params;
 
